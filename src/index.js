a685a2d5841a5c5c95269426972d5d63d1944d73
Joe <54695266+jdboris@users.noreply.github.com>
2023-05-27 03:21:45 -0400

    Add so-called "Activity Bar" on the left

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "@jdboris/css-themes/vs-code";
+// import "@vscode/codicons/dist/codicon.css";
 import "spa-routing";
 import "./style.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
+        test: /\.(sa|sc|c)ss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
