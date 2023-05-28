4fcca7e0a028c70bbaa734bc0782fecd3c1f8ddd
Joe <54695266+jdboris@users.noreply.github.com>
2023-05-28 00:00:58 -0400

    Fix dist folder path in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "/dist"),
+    path: path.resolve(__dirname, "./dist"),
     // Source: https://stackoverflow.com/a/50179280
     publicPath: "/",
   },
