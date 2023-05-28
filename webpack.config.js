9f4db89249edaaeec75d2a8cfc84703e99d303a2
Joe <54695266+jdboris@users.noreply.github.com>
2023-05-28 11:32:44 -0400

    Add environment variables accessible in src

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    // Source: https://stackoverflow.com/a/50179280
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ],
-  devServer: {
-    static: path.resolve(__dirname, "src"),
-    port: 8080,
-    open: true,
-    hot: true,
-    // Source: https://stackoverflow.com/a/50179280
-    historyApiFallback: {
-      index: "/",
+module.exports = ({ NODE_ENV }) => {
+  require("dotenv").config({
+    path: NODE_ENV == "production" ? ".env.production" : ".env",
+  });
+
+  return {
+    entry: "./src/index.js",
+    mode: NODE_ENV,
+    output: {
+      // NOTE: Remove the '/' from the beginning
+      filename: `${(process.env.APP_PATH || "").substring(1)}bundle.js`,
+      path: path.resolve(__dirname, "./dist"),
+      // Source: https://stackoverflow.com/a/50179280
+      publicPath: "/",
     },
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env),
+      }),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+      }),
+    ],
+    devServer: {
+      static: path.resolve(__dirname, "src"),
+      port: 8080,
+      open: true,
+      hot: true,
+      // Source: https://stackoverflow.com/a/50179280
+      historyApiFallback: {
+        index: "/",
+      },
+    },
+  };
 };
