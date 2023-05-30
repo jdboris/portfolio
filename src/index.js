e8f44fe3bdb531e5a0cf94171b6e17423718b3a9
Joe <54695266+jdboris@users.noreply.github.com>
2023-05-30 15:22:17 -0400

    Fix fontawesome imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "@jdboris/css-themes/vs-code";
 import { setRoot } from "spa-routing";
+import "@fortawesome/fontawesome-free/css/all.css";
+
 import "./style.scss";
 
 setRoot(process.env.APP_PATH || "/");
