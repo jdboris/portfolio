d47e20299283c2d4d9f8f35b400b93f67c486355
Joe <54695266+jdboris@users.noreply.github.com>
2023-06-02 08:01:18 -0400

    Make navbar close on smaller screens

--- src/index.js
+++ src/index.js
@@ -4,3 +4,20 @@ import "@fortawesome/fontawesome-free/css/all.css";
 import "./style.scss";
 
 setRoot(process.env.APP_PATH || "/");
+
+const NAV_SIZE_BREAKPOINT = 645;
+let previousWidth = null;
+
+new ResizeObserver((entries) => {
+  entries.forEach((entry) => {
+    if (
+      entry.contentRect.width <= NAV_SIZE_BREAKPOINT &&
+      (previousWidth > NAV_SIZE_BREAKPOINT || previousWidth === null)
+    ) {
+      document
+        .querySelectorAll("body > main > nav > details[open]")
+        .forEach((x) => (x.open = false));
+    }
+    previousWidth = entry.contentRect.width;
+  });
+}).observe(document.body);
